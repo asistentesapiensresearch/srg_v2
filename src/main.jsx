36366f2f0e24d75fc6fa75e6232f3acb97dec30b
lBrayan <lbrayan.roa@gmail.com>
2025-11-12 15:56:54 -0500

    Fix deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 
 import App from './App'; 
-/*
+
 import { Amplify } from 'aws-amplify';
 import { I18n, parseAmplifyConfig } from "aws-amplify/utils";
 import outputs from '../amplify_outputs.json';
@@ -16,7 +16,6 @@ I18n.putVocabularies(translations);
 I18n.setLanguage('es');
 
 Amplify.configure(amplifyConfig);
-*/
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
