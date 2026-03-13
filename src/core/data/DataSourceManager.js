cece2e1be0233956c5c6dc6a16cf2ff0562110a6
Héctor Alejandro Toro Bernal <66646440+hatorob@users.noreply.github.com>
2026-03-12 19:29:00 -0500

    feact: agrego DataSourceManager para que las peticiones hacia los modelos esten controlados, adicional con permanecia de cache para que no haga más de una vez la petición cuando busca por la misma petición

---
+++ src/core/data/DataSourceManager.js
@@ -0,0 +1,52 @@
+import { generateClient } from 'aws-amplify/data';
+
+const client = generateClient();
+
+class DataSourceManager {
+
+    static cache = {};
+    
+    static getCacheKey(type, params) {
+        return `${type}:${JSON.stringify(params)}`;
+    }
+
+    // Métodos para buscar en la BD usando Amplify DataStore
+    static async findByField(modelName, searchField, searchValue, limit = 1) {
+
+        const key = this.getCacheKey("db-field", { modelName, searchField, searchValue, limit });
+        if (this.cache[key]) {
+            return this.cache[key];
+        }
+
+        const promise =  client.models[modelName].list({
+            filter: { [searchField]: { eq: searchValue } },
+            limit
+        });
+
+
+        this.cache[key] = promise;
+
+        return promise;
+    }
+
+    static async findWithFilter(modelName, filter = {}, limit = 1) {
+
+        const key = this.getCacheKey("db-filter", { modelName, filter, limit });
+
+        if (this.cache[key]) {
+            return this.cache[key];
+        }
+
+        const promise = client.models[modelName].list({
+            filter: Object.keys(filter).length ? filter : undefined,
+            limit
+        });
+
+        this.cache[key] = promise;
+
+        return promise;
+    }
+
+}
+
+export default DataSourceManager;
