1dce54517c02855469cd48e54b55be7c5ab003aa
Héctor Alejandro Toro Bernal <66646440+hatorob@users.noreply.github.com>
2026-04-08 12:40:48 -0500

    feat: agrego las opciones según los archivos del excel para cuando se tare la data como para los componentes que por el momento hacen uso de el

--- src/pages/admin/components/helpers/sections/DowloandDataExcel/schema.js
+++ src/pages/admin/components/helpers/sections/DowloandDataExcel/schema.js
@@ -30,10 +30,30 @@ export default {
         },
         {
             name: "identifierExcel",
-            label: "Un identificador único para el Excel, es obliggatorio para poder consultar los datos del excel",
-            type: "text",
-            default: "",
-            help: "Puede ser la hoja seleccionada, ej: COL, M-TOP, etc"
+            label: "Seleccionar Fuente de Datos Archivo Excel para su identificador interno",
+            type: "select",
+            default: "M-TOP",
+            options: [
+                { label: "U-Sapiens", value: "U" },
+                { label: "ASC-Sapiens", value: "ASC" },
+                { label: "Mejores ASC", value: "M-ASC" },
+                { label: "ART-Sapiens", value: "ART" },
+                { label: "Mejores ART", value: "M-ART" },
+                { label: "DTI-Sapiens", value: "DTI" },
+                { label: "Mejores DTI", value: "M-DTI" },
+                { label: "GNC-Sapiens", value: "GNC" },
+                { label: "Mejores GNC", value: "M-GNC" },
+                { label: "FRH-Sapiens", value: "FRH" },
+                { label: "Mejores FRH", value: "M-FRH" },
+                { label: "REV-Sapiens", value: "REV" },
+                { label: "POST-Sapiens", value: "POST" },
+                { label: "PRE-Sapiens", value: "PRE" },
+                { label: "Col-Sapiens", value: "Col" },
+                { label: "100 Mejores por Materia", value: "M-TOP" },
+                { label: "Micrositios / Universidades / Encuentra / Grupos", value: "E-Grupos"},
+                { label: "Micrositios / Universidades / Encuentra / Revistas", value: "E-Revistas"},
+                { label: "Micrositios / Colegios / Posiciones", value: "Micro-Posionamiento"}
+            ],
         },
     ]
 };
