045adbee2e9e70a98324622ab1b70fb6344f6867
Héctor Alejandro Toro Bernal <66646440+hatorob@users.noreply.github.com>
2026-04-13 10:36:56 -0500

    refactor: Agrego las opciones faltantes en CalificationAndCategory

--- src/pages/admin/components/helpers/sections/CalificationAndCategory/schema.js
+++ src/pages/admin/components/helpers/sections/CalificationAndCategory/schema.js
@@ -9,8 +9,25 @@ export default {
             type: "select",
             default: "M-TOP",
             options: [
-                { label: "COLSAPIENS", value: "COL" },
-                { label: "Mejores 100", value: "M-TOP" }
+                { label: "U-Sapiens", value: "U" },
+                { label: "ASC-Sapiens", value: "ASC" },
+                { label: "Mejores ASC", value: "M-ASC" },
+                { label: "ART-Sapiens", value: "ART" },
+                { label: "Mejores ART", value: "M-ART" },
+                { label: "DTI-Sapiens", value: "DTI" },
+                { label: "Mejores DTI", value: "M-DTI" },
+                { label: "GNC-Sapiens", value: "GNC" },
+                { label: "Mejores GNC", value: "M-GNC" },
+                { label: "FRH-Sapiens", value: "FRH" },
+                { label: "Mejores FRH", value: "M-FRH" },
+                { label: "REV-Sapiens", value: "REV" },
+                { label: "POST-Sapiens", value: "POST" },
+                { label: "PRE-Sapiens", value: "PRE" },
+                { label: "Col-Sapiens", value: "Col" },
+                { label: "100 Mejores por Materia", value: "M-TOP" },
+                { label: "Micrositios / Universidades / Encuentra / Grupos", value: "E-Grupos"},
+                { label: "Micrositios / Universidades / Encuentra / Revistas", value: "E-Revistas"},
+                { label: "Micrositios / Colegios / Posiciones", value: "Micro-Posionamiento"}
             ],
         },
         //personalizacion de los estilos
